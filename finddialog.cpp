a0f29c702e8441647e53b8dbd857774a3f95260f
Lixinhang2021051615007 <1551467563@qq.com>
2023-03-21 23:42:00 +0800

    FindDialog 3rd.Li

---
+++ finddialog.cpp
@@ -0,0 +1,103 @@
+/**
+ * author:Lixinhang
+ * date:2023.03
+ *
+ * the implementation of class FindDialog using modern c++ and Qt6
+*/
+#include "finddialog.h"
+#include <QtWidgets>
+#include <QtDebug>
+
+FindDialog::FindDialog(QWidget *parent)
+    : QDialog(parent)
+{
+    QLabel * l = new QLabel(tr("Find &what"));
+    l->setObjectName(QString{"label"});
+
+    QLineEdit *le = new QLineEdit();
+    le->setObjectName(QString{"lineEdit"});
+
+    l->setBuddy(le);
+
+    QPushButton *fb = new QPushButton(tr("Find"));
+    fb->setObjectName(QString{"findButton"});
+
+    QPushButton *cb = new QPushButton(tr("Close"));
+    cb->setObjectName(QString{"closeButton"});
+
+    fb -> setEnabled(false);
+    fb -> setDefault(true);
+
+    connect(le, &QLineEdit::textChanged,
+            this, &FindDialog::enableFindButton);
+    connect(fb, &QPushButton::clicked,
+            this, &FindDialog::findClicked);
+    connect(cb, &QPushButton::clicked,
+            this, &FindDialog::close);
+
+    QCheckBox *caseCheckBox = new QCheckBox(tr("Match &case"));
+    caseCheckBox->setObjectName(QString{"caseCheckBox"});
+
+    QCheckBox *backwardCheckBox = new QCheckBox(tr("Search &backward"));
+    backwardCheckBox->setObjectName(QString{"backwardCheckBox"});
+
+    QHBoxLayout *topleftLayout = new QHBoxLayout();
+    topleftLayout ->addWidget(l);
+    topleftLayout ->addWidget(le);
+
+    QVBoxLayout *leftLayout = new QVBoxLayout();
+    leftLayout ->addLayout(topleftLayout);
+    leftLayout ->addWidget(caseCheckBox);
+    leftLayout ->addWidget(backwardCheckBox);
+
+    QVBoxLayout *rightLayout = new QVBoxLayout();
+    rightLayout ->addWidget(fb);
+    rightLayout ->addWidget(cb);
+    rightLayout ->addStretch();
+
+    QHBoxLayout *mainLayout = new QHBoxLayout();
+    mainLayout ->addLayout(leftLayout);
+    mainLayout ->addLayout(rightLayout);
+
+    setLayout(mainLayout);
+
+    setWindowTitle(tr("Find"));
+    setFixedHeight(sizeHint().height());
+}
+
+FindDialog::~FindDialog()
+{
+
+}
+
+void FindDialog::enableFindButton(const QString &text)
+{
+    QPushButton *findButton = findChild<QPushButton*>(QString{"findButton"});
+    findButton ->setEnabled(!(text.isEmpty()));
+}
+
+void FindDialog::findClicked()
+{
+    QCheckBox *caseCheckBox = findChild<QCheckBox*>(QString{"caseCheckBox"});
+    Qt::CaseSensitivity cs =
+            caseCheckBox->isChecked() ? Qt::CaseSensitive
+                                       : Qt::CaseInsensitive;
+    if(caseCheckBox->isChecked())
+        cs = Qt::CaseSensitive;
+    else
+        cs = Qt::CaseInsensitive;
+
+    QLineEdit *lineEdit = findChild<QLineEdit *>(QString{"lineEdit"});
+    QString text = lineEdit->text();
+
+    QCheckBox *backwardCheckBox = findChild<QCheckBox *>(QString{"backwardCheckBox"});
+    if(backwardCheckBox->isChecked()){
+        emit findPrevious(text, cs);
+        qDebug() << "Singal findPrevious is emitted.";
+    }
+    else{
+        emit findNext(text, cs);
+        qDebug() << "Singal findNext is emitted.";
+    }
+
+}
---
+++ finddialog.h
@@ -0,0 +1,40 @@
+/**
+ * author:Lixinhang
+ * date:2020.03
+ * the Declaration of class FindDialog
+*/
+#ifndef FINDDIALOG_H
+#define FINDDIALOG_H
+
+#include <QDialog>
+
+//class QLabel;
+//class QLineEdit;
+//class QCheckBox;
+//class QPushButton;
+//class QString;
+
+class FindDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    FindDialog(QWidget *parent = nullptr);
+    ~FindDialog();
+signals:
+    void findNext(const QString &str, Qt::CaseSensitivity cs);
+    void findPrevious(const QString &str, Qt::CaseSensitivity cs);
+
+private slots:
+    void enableFindButton(const QString &text);
+    void findClicked();
+
+//private:
+//    QLabel *_label = nullptr;
+//    QLineEdit *_lineEdit = nullptr;
+//    QCheckBox *_caseCheckBox = nullptr;
+//    QCheckBox *_backwardCheckBox = nullptr;
+//    QPushButton *_findButton = nullptr;
+//    QPushButton *_closeButton = nullptr;
+};
+
+#endif // FINDDIALOG_H
