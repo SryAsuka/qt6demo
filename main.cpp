fd793a394b3adce2f984d441995af2c0802756c0
Lixinhang2021051615007 <1551467563@qq.com>
2023-03-21 23:44:53 +0800

    GotoCell is designed by Qt Designer

--- main.cpp
+++ main.cpp
@@ -1,19 +1,17 @@
-/*
- * 
- * author: Lixinhang
- * date:2023.03
- * verstion 1.0
- *
- */
-#include "finddialog.h"
+//author : Linxinhang
+//date : 2023.3.15
+//project : GotoCell is designed by Qt Designer
+//version 0.1.0
+
+#include "dialog.h"
+
 #include <QApplication>
 #include <memory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    auto window {std::make_unique<FindDialog>()};
+    auto window {std::make_unique<Dialog>()};
     window->show();
-
     return a.exec();
 }
